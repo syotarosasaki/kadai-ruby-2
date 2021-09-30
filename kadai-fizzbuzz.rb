74ac4be43dd5bbf886e73bec731775c6ae830141
syotaro.sasaki <ssk.syotaro.0921@gmail.com>
2021-09-30 10:27:10 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,18 @@
 def fizzbuzz(num)
     if num%15==0
-        puts "FizzBuzz"
+        return "FizzBuzz"
     elsif num%3==0
-        puts "Fizz"
+        return "Fizz"
     elsif num%5==0
-        puts "Buzz"
+        return "Buzz"
     else
-        puts num
+        return num
     end
 end
 
 num_max = 100
 (1..num_max).each do |num|
-  puts fizzbuzz(num)
+    puts fizzbuzz(num)
 end
 
     
